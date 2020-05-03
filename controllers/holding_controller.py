eaee8ffc26f4571e0bdd8521b3a58dcc7a2e10ae
MikeElias14 <michael@site469.com>
2020-05-03 17:06:03 -0400

    send more relevant data

--- controllers/holding_controller.py
+++ controllers/holding_controller.py
@@ -1,9 +1,16 @@
 import yfinance as yf
+from models.holding_model import unused_info
 
 
 def get_info(ticker):
     ticker = yf.Ticker(ticker)
-    return ticker.info
+    info = ticker.info
+
+    # Get rid of things I don't currently use
+    for field in unused_info:
+        info.pop(field)
+
+    return info
 
 
 def get_div(ticker):
