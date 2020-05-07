f27b5ffabfdfc0e58277bb42bd9fff03281c1624
MikeElias14 <michael@site469.com>
2020-05-07 15:43:38 -0400

    bug fix when bug happens

--- controllers/holding_controller.py
+++ controllers/holding_controller.py
@@ -8,21 +8,22 @@ def get_info(tickers, index=False):
         yf_ticker = yf.Ticker(ticker)
         try:
             ticker_info = yf_ticker.info
+
+            # Get rid of things I don't currently use
+            if index:
+                remove_info = unused_index_info
+            else:
+                remove_info = unused_info
+
+            for field in remove_info:
+                if field in ticker_info:
+                    ticker_info.pop(field)
+
+            info.append(dict(ticker=ticker, info=ticker_info))
+
         except:
             ticker_info = "Bug"  # TODO: Fix this, https://github.com/ranaroussi/yfinance/issues/208
             info.append(dict(ticker=ticker, info=ticker_info))
-            break
-
-        # Get rid of things I don't currently use
-        if index:
-            remove_info = unused_index_info
-        else:
-            remove_info = unused_info
-
-        for field in remove_info:
-            if field in ticker_info:
-                ticker_info.pop(field)
-        info.append(dict(ticker=ticker, info=ticker_info))
 
     return info
 
@@ -33,15 +34,16 @@ def get_div(tickers):
         yf_ticker = yf.Ticker(ticker)
         try:
             ticker_div = yf_ticker.dividends.to_dict()
-        except:
-            ticker_div = "Bug"  # TODO: Fix this, https://github.com/ranaroussi/yfinance/issues/208
-            divideds.append(dict(ticker=ticker, div=ticker_div))
-            break
 
-        dict_div = []
-        for obj in ticker_div:
-            dict_div.append({obj.strftime('%Y/%m/%d'): ticker_div[obj]})
-        divideds.append(dict(ticker=ticker, div=dict_div))
+            dict_div = []
+            for obj in ticker_div:
+                dict_div.append({obj.strftime('%Y/%m/%d'): ticker_div[obj]})
+
+            divideds.append(dict(ticker=ticker, history=dict_div))
+
+        except:
+            ticker_div = [dict(bug=1)]  # TODO: Fix this, https://github.com/ranaroussi/yfinance/issues/208
+            divideds.append(dict(ticker=ticker, history=ticker_div))
 
     return divideds
 
@@ -52,6 +54,7 @@ def get_events(tickers):
         yf_ticker = yf.Ticker(ticker)
         try:
             ticker_events = yf_ticker.calendar.to_dict()
+
         except:
             ticker_events = "Bug"  # TODO: Fix this, https://github.com/ranaroussi/yfinance/issues/208
 
@@ -67,18 +70,18 @@ def get_history(tickers, time, interval):
 
         try:
             history = yf_ticker.history(period=time, interval=interval, prepost=False, actions=False).to_dict()
+
+            for field in unused_history:
+                if field in history:
+                    history.pop(field)
+
+            ticker_history = []
+            for obj in history['Close']:
+                ticker_history.append({obj.strftime('%Y/%m/%d'): history['Close'][obj]})
+            tickers_history.append(dict(ticker=ticker, history=ticker_history))
+
         except:
             history = "Bug"  # TODO: Fix this, https://github.com/ranaroussi/yfinance/issues/208
             tickers_history.append(dict(ticker=ticker, history=history))
-            break
-
-        for field in unused_history:
-            if field in history:
-                history.pop(field)
-
-        ticker_history = []
-        for obj in history['Close']:
-            ticker_history.append({obj.strftime('%Y/%m/%d'): history['Close'][obj]})
-        tickers_history.append(dict(ticker=ticker, history=ticker_history))
 
     return tickers_history
